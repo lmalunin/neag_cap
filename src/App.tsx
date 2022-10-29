357355be3da776d73f7d75699df69b5babe17176
lmalunin <lmalunin@gmail.com>
2022-10-29 19:29:10 +0300

    75

--- src/App.tsx
+++ src/App.tsx
@@ -2,44 +2,46 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App = () => (
-    <div className="categories-container">
-        <div className="category-container">
-            {/*img*/}
-            <div className="category-body-container">
-                <h2>hats</h2>
-                <h2>shop now</h2>
-            </div>
+const App = () => {
+
+    const categories = [
+        {
+            id: 1,
+            title: 'Hats',
+        },
+        {
+            id: 2,
+            title: 'Jackets',
+        },
+        {
+            id: 3,
+            title: 'Sneakers',
+        },
+        {
+            id: 4,
+            title: 'Womens',
+        },
+        {
+            id: 5,
+            title: 'Mens',
+        }
+    ];
+
+    return (
+        <div className="categories-container">
+            {
+                categories.map(({id, title}) => (
+                    <div className="category-container">
+                        <div className="background-image"></div>
+                        <div className="category-body-container">
+                            <h2>hats</h2>
+                            <h2>shop now</h2>
+                        </div>
+                    </div>
+                ))
+            }
         </div>
-        <div className="category-container">
-            {/*img*/}
-            <div className="category-body-container">
-                <h2>jakets</h2>
-                <h2>shop now</h2>
-            </div>
-        </div>
-        <div className="category-container">
-            {/*img*/}
-            <div className="category-body-container">
-                <h2>sneakers</h2>
-                <h2>shop now</h2>
-            </div>
-        </div>
-        <div className="category-container">
-            {/*img*/}
-            <div className="category-body-container">
-                <h2>womens</h2>
-                <h2>shop now</h2>
-            </div>
-        </div>
-        <div className="category-container">
-            {/*img*/}
-            <div className="category-body-container">
-                <h2>mens</h2>
-                <h2>shop now</h2>
-            </div>
-        </div>
-    </div>
-);
+    );
+}
 
 export default App;
