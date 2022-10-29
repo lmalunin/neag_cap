e2b941556ce48cafae1a161374bbf574125d84e4
lmalunin <lmalunin@gmail.com>
2022-10-29 13:39:40 +0300

    74

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,34 @@ const App = () => (
                 <h2>shop now</h2>
             </div>
         </div>
+        <div className="category-container">
+            {/*img*/}
+            <div className="category-body-container">
+                <h2>jakets</h2>
+                <h2>shop now</h2>
+            </div>
+        </div>
+        <div className="category-container">
+            {/*img*/}
+            <div className="category-body-container">
+                <h2>sneakers</h2>
+                <h2>shop now</h2>
+            </div>
+        </div>
+        <div className="category-container">
+            {/*img*/}
+            <div className="category-body-container">
+                <h2>womens</h2>
+                <h2>shop now</h2>
+            </div>
+        </div>
+        <div className="category-container">
+            {/*img*/}
+            <div className="category-body-container">
+                <h2>mens</h2>
+                <h2>shop now</h2>
+            </div>
+        </div>
     </div>
 );
 
