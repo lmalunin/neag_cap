0f93173817a65582f032fef5e22d37616d48cfb2
lmalunin <lmalunin@gmail.com>
2022-10-30 17:16:48 +0300

    082 Setting Up Our Homepage

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import './fonts/Roboto/Roboto-Light.ttf';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom'
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
         <React.StrictMode>
-            <App/>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
         </React.StrictMode>
 );
 
