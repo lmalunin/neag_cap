a8cd657cc3a5a079444e83eefc626499d8d1130e
lmalunin <lmalunin@gmail.com>
2022-11-01 18:23:01 +0300

    095 Sign In With Redirect

--- src/routes/sign-in/sign-in.component.tsx
+++ src/routes/sign-in/sign-in.component.tsx
@@ -1,9 +1,34 @@
-import {createUserDocumentFromAuth, signInWithGooglePopup} from "../../utils/firebase/frebase.utils";
+import {useEffect} from "react";
+import {getRedirectResult} from 'firebase/auth';
+
+
+import {
+    auth,
+    createUserDocumentFromAuth,
+    signInWithGooglePopup,
+    signInWithGoogleRedirect
+} from "../../utils/firebase/frebase.utils";
 
 const SignIn = () => {
+
+    useEffect(() => {
+        let response;
+        (async () => {
+            response = await getRedirectResult(auth);
+            if (response) {
+                const userDocRef = await createUserDocumentFromAuth(response.user);
+            }
+        })();
+    }, []);
+
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
+    }
+
+    const logGoogleRedrectUser = async () => {
+        const {user} = await signInWithGoogleRedirect();
+        console.log(user);
     }
 
     return (
@@ -11,6 +36,9 @@ const SignIn = () => {
                 <h1>Sing in page</h1>
                 <button onClick={() => logGoogleUser()}>
                     Sign in with google popup
+                </button>
+                <button onClick={() => signInWithGoogleRedirect()}>
+                    Sign in with google redirect
                 </button>
             </div>
     )
