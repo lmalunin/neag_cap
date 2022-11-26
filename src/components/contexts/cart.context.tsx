322df905399d23874571764729cec6e8cb1894cf
lmalunin <lmalunin@gmail.com>
2022-11-26 18:19:57 +0300

    119 Cart Item Designs

--- src/components/contexts/cart.context.tsx
+++ src/components/contexts/cart.context.tsx
@@ -1,4 +1,4 @@
-import {createContext, Dispatch, useState} from "react";
+import {createContext, Dispatch, useEffect, useState} from "react";
 
 export type CategoryItem = {
     id: number;
@@ -20,7 +20,7 @@ const addCartItem = (cartItems, productToAdd): CartItem[] => {
                 ? {...cartItem, quantity: cartItem.quantity + 1}
                 : cartItem)
     }
-    
+
     return [...cartItems, {...productToAdd, quantity: 1}];
 }
 
@@ -29,18 +29,36 @@ export const CartContext = createContext({
     setIsCartOpen: (() => undefined) as Dispatch<any>,
     cartItems: new Array<CartItem>(),
     addItemToCart: (() => undefined) as Dispatch<any>,
+    cartCount: 0,
+    setCartCount: (() => undefined) as Dispatch<any>,
 })
 
 export const CartProvider = ({children}) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState(new Array<CartItem>());
+    const [cartCount, setCartCount] = useState(0);
 
     const addItemToCart = (productToAdd) => {
         const res: CartItem[] = addCartItem(cartItems, productToAdd)
         setCartItems(res);
     }
 
-    const value = {isCartOpen, setIsCartOpen, cartItems, addItemToCart};
+    useEffect(() => {
+        const newCartCount = cartItems.reduce(
+                (total, cartItem) => total + cartItem.quantity, 0
+        )
+        setCartCount(newCartCount);
+
+    }, [cartItems])
+
+    const value = {
+        isCartOpen,
+        setIsCartOpen,
+        cartItems,
+        addItemToCart,
+        cartCount,
+        setCartCount
+    };
 
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
