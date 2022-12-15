1a38cf98d9c9e844a59526d3b49e07f6167e7402
lmalunin <lmalunin@gmail.com>
2022-12-15 11:30:54 +0300

    135 Introducing Styled-Components.mp4

--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -1,38 +1,38 @@
-import {Link, Outlet} from "react-router-dom";
-import React, {useContext} from "react";
-import {ReactComponent as CrwnLogo} from "../../assets/crown.svg";
-import './navigation.styles.scss';
-import {UserContext} from "../../components/contexts/user.context";
-import {signOutUser} from "../../utils/firebase/frebase.utils";
+import { Link, Outlet } from "react-router-dom";
+import React, { useContext } from "react";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import { NavigationContainer, NavLinks, NavLink, LogoContainer } from './navigation.styles';
+import { UserContext } from "../../components/contexts/user.context";
+import { signOutUser } from "../../utils/firebase/frebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
-import {CartContext} from "../../components/contexts/cart.context";
+import { CartContext } from "../../components/contexts/cart.context";
 
 const Navigation = () => {
 
-    const {currentUser} = useContext(UserContext);
-    const {isCartOpen} = useContext(CartContext);
+    const { currentUser } = useContext(UserContext);
+    const { isCartOpen } = useContext(CartContext);
 
     return (
-            <>
-                <div className='navigation'>
-                    <Link className='logo-container' to='/'>
-                        <CrwnLogo className='logo'></CrwnLogo>
-                    </Link>
-                    <div className='nav-links-container'>
-                        <Link className='nav-link' to='/shop'>Shop</Link>
-                        {
-                            currentUser
-                                    ? (<span className='nav-link' onClick={signOutUser}>Sign Out</span>)
-                                    : (<Link className='nav-link' to='/auth'>Sign in</Link>)
-                        }
-                        <CartIcon/>
-                    </div>
-                    {isCartOpen && <CartDropdown/>}
-                </div>
-                <Outlet/>
-            </>
+        <>
+            <NavigationContainer>
+                <LogoContainer to='/'>
+                    <CrwnLogo className='logo'></CrwnLogo>
+                </LogoContainer>
+                <NavLinks>
+                    <NavLink to='/shop'>Shop</NavLink>
+                    {
+                        currentUser
+                            ? (<NavLink as='span' onClick={signOutUser}>Sign Out</NavLink>)
+                            : (<NavLink to='/auth'>Sign in</NavLink>)
+                    }
+                    <CartIcon/>
+                </NavLinks>
+                {isCartOpen && <CartDropdown/>}
+            </NavigationContainer>
+            <Outlet/>
+        </>
     );
 }
 
-export {Navigation};
+export { Navigation };
