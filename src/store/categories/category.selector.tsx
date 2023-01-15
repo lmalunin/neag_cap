497ab2c2d6776e120239a16d4385b6373a9c06cc
lmalunin <lmalunin@gmail.com>
2023-01-15 16:15:33 +0300

    161 Reselect Library.mp4

--- src/store/categories/category.selector.tsx
+++ src/store/categories/category.selector.tsx
@@ -1,9 +1,21 @@
-export const selectCategoriesMap = (state: any) => state?.categories.categories
-    .reduce((acc: any, category) => {
-        const { title, items } = category;
-        if (title) {
-            acc[title?.toLowerCase()] = items;
-        }
-        return acc;
-    }, {});
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice) => categoriesSlice.categories
+)
+
+export const selectCategoriesMap = createSelector(
+    [selectCategories],
+    categories =>
+        categories.reduce((acc: any, category) => {
+            const { title, items } = category;
+            if (title) {
+                acc[title?.toLowerCase()] = items;
+            }
+            return acc;
+        }, {})
+);
 
