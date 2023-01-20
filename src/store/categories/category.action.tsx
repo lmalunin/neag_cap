b09a3f8a027e78637dab291a0cbe954067422c71
lmalunin <lmalunin@gmail.com>
2023-01-20 15:22:41 +0300

    169 Redux-Thunk Pt. 2.mp4 cleanup

--- src/store/categories/category.action.tsx
+++ src/store/categories/category.action.tsx
@@ -2,9 +2,6 @@ import { getCategoriesAndDocuments } from "../../utils/firebase/frebase.utils";
 import { createAction } from "../../utils/reducer.utils";
 import { CATEGORIES_ACTION_TYPES } from "./category.types";
 
-// export const setCategories = (categories) =>
-//     createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categories);
-
 export const fetchCategoriesStart = () => createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
 export const fetchCategoriesSuccess = (payload) => createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, payload);
 export const fetchCategoriesError = (error) => createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, error);
