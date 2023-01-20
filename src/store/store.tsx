7bc920bf62ec6e389bc39a549962ac9e0c6ece9b
lmalunin <lmalunin@gmail.com>
2023-01-20 14:36:15 +0300

    168 Asynchronous Redux_ Redux-Thunk.mp4

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -3,7 +3,7 @@ import logger from 'redux-logger';
 import { rootReducer } from "./root.reducer";
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-
+import thunk from "redux-thunk";
 
 const persistConfig = {
     key: 'root',
@@ -13,7 +13,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+const middleWares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean);
 const composeEnhancer = (process.env.NODE_ENV != 'production' && window && window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__']) || compose;
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
