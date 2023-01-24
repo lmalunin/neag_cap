2b72b36313b93b8864b589ff76dca35676e1535a
lmalunin <lmalunin@gmail.com>
2023-01-24 11:25:33 +0300

    171 Asynchronous Redux_ Redux-Saga.mp4

--- src/store/root.reducer.tsx
+++ src/store/root.reducer.tsx
@@ -2,15 +2,6 @@ import { combineReducers } from 'redux';
 import { cartReducer } from "./cart/cart.reducer";
 import { categoriesReducer } from "./categories/category.reducer";
 import { userReducer } from "./user/user.reducer";
-import { configureStore } from '@reduxjs/toolkit'
-
-const store = configureStore({
-    reducer: {
-        user: userReducer,
-        categories: categoriesReducer,
-        cart: cartReducer,
-    }
-})
 
 export const rootReducer = combineReducers({
     user: userReducer,
